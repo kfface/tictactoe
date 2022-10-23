152678a6326d719e6bd60b9ae8090fd15da78a50
Kolbe Face <kfface@gmail.com>
2022-10-23 10:53:19 -0700

    Forced user to make a legal move
    found bug that allowed user to break program by not selecting int.  Also found bug which allowed user to override a previous computer move.  Both are fixed with this commit

--- ttt.py
+++ ttt.py
@@ -23,9 +23,16 @@ def printBoard():
     print()
 
 def userMove():
-    selection = int(input("Your turn where would you like to play?: "))
-    while selection > 10 or selection < 0:
-        selection = int(input("that move is invalid please choose a legal move: "))
+    gettingInput = True
+    while gettingInput:
+        try:
+            selection = int(input("Your turn where would you like to play?: "))
+            while selection > 10 or selection < 1 or positions[selection] == "X" or positions[selection] == "O":
+                selection = int(input("that move is invalid please choose a legal move: "))
+            if selection < 10 and selection > 0:
+                gettingInput = False
+        except ValueError:
+            print("Please select a space on the board 1-9")
     while positions[selection] == "X" or  selection > 9 or selection < 0:
         selection = int(input("that move is invalid please choose a legal move: "))
     else:
