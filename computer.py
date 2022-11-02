fc738d5d11b3806c52a9ff918e4c0182998ac909
Kolbe Face <kfface@gmail.com>
2022-11-01 19:20:51 -0700

    Objectified and created multiple files
    aki said i needed to make much more use of objects and that my whole app should not be one file

---
+++ computer.py
@@ -0,0 +1,56 @@
+import board.py
+class Computer:
+    #picks a 'random' move when no block or win move is available
+    #the middle space has the highest weight followed by the four corners
+    def computerStarterMoves (self):
+        global winCondList
+        priorityMoveAvailable = False
+        priorityMoves = [5,1,3,7,9]
+        for value in priorityMoves:
+            if positions[value] != "X" and positions[value] != "O":
+                compMove = value
+                positions[compMove] = "O"
+                priorityMoveAvailable = True
+                break
+        if priorityMoveAvailable == False:
+            compMove = random.choice(list(positions))
+            while positions[compMove] == "O" or positions[compMove] == "X":
+                compMove = random.choice(list(positions))
+                positions[compMove] = "O"
+        winCondList = [list(map(lambda y: y if y != compMove else 'O', i)) for i in winCondList]
+
+    def computerMoveWinOrBlock (self):
+        global noBestMove
+        global userTurn
+        global winCondList
+        global compMove
+        for list in winCondList:
+            xCounter = 0
+            oCounter = 0
+            if userTurn == True:
+                break
+            for value in list:
+                if value == "O":
+                    oCounter += 1
+                if oCounter == 2:
+                    for index, item in enumerate(list):
+                        if list[index] != "O" and list[index] != "X":
+                            compMove = list[index]
+                            positions[list[index]] = "O"
+                            noBestMove = False
+                            userTurn = True
+        for list in winCondList:
+            xCounter = 0
+            oCounter = 0
+            if userTurn == True:
+                break    
+            for value in list:
+                if value == "X":
+                    xCounter += 1
+                if xCounter == 2:
+                    for index, item in enumerate(list):
+                        if list[index] != "O" and list[index] != "X":
+                            compMove = list[index]
+                            positions[list[index]] = "O"
+                            noBestMove = False
+                            userTurn = True
